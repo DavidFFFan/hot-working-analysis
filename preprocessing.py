76a4ead9db0e8c98291e083552aa880a9517efe9
DavidFFFan <DavidFFFan@foxmail.com>
2020-09-14 19:41:43 +0800

    增加全局变量starT和endT，函数参数由字符串改为时间类型。

--- preprocessing.py
+++ preprocessing.py
@@ -2,22 +2,20 @@ import datetime
 import pandas as pd
 import numpy as np
 from typing import List
+startT = endT = None
 
-def minNums(startTime: str, endTime: str) -> int:
+
+def minNums(startTime, endTime) -> int:
     '''计算两个时间点之间的分钟数'''
-    startTime1 = datetime.datetime.strptime(startTime, r"%Y/%m/%d %H:%M:%S")
-    endTime1 = datetime.datetime.strptime(endTime, r"%Y/%m/%d %H:%M:%S")
-    # seconds = (endTime1 - startTime1).seconds
-    # 来获取时间差中的秒数.seconds获得的秒只是时间差中的小时、分钟和秒部分的和，并没有包含时间差的天数（既是两个时间点不是同一天，失效）
-    total_seconds = int((endTime1 - startTime1).total_seconds()
-                        ) + startTime1.second - endTime1.second
+    total_seconds = int((endTime - startTime).total_seconds()
+                        ) + startTime.second - endTime.second
     return total_seconds // 60
 
-def getTime(startTime: str, num: int) -> str:
-    startTime1 = datetime.datetime.strptime(startTime, r"%Y/%m/%d %H:%M:%S")
-    time = startTime1 + datetime.timedelta(minutes=num)
 
-    return time.strftime(r"%Y/%m/%d %H:%M:%S")
+def getTime(num: int, startTime) -> str:
+    time = startTime + datetime.timedelta(minutes=num)
+    return time
+
 
 def interpolation(data: List[int]):
     ''' 窗口为3的平均插值函数'''
@@ -34,21 +32,24 @@ def load_csv_data(path: str) -> List[int]:
     corpus = pd.read_csv(
         path, usecols=["NOW_TIME", "CONTROLTEMP1_CURRENT_T"])
 
-    
     # 转为list
     corpus = corpus.values.tolist()
-    # print(corpus)
+    
     start, end = corpus[0][0], corpus[-1][0]
+    global startT, endT
+    startT = datetime.datetime.strptime(start, r"%Y/%m/%d %H:%M:%S")
+    endT = datetime.datetime.strptime(end,   r"%Y/%m/%d %H:%M:%S")
 
     # 0-1有两个时刻，因此要+1
-    size = minNums(start, end) + 1
+    size = minNums(startT, endT) + 1
     print('size = ', size)
 
     tdata = np.zeros(size, dtype='int')
 
     # 转换为从0时刻开始
     for c in corpus:
-        tdata[minNums(start, c[0])] = c[1]
+        t = datetime.datetime.strptime(c[0], r"%Y/%m/%d %H:%M:%S")
+        tdata[minNums(startT, t)] = c[1]
 
     # 插值，去掉0
     interpolation(tdata)
