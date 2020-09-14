ca22030122c2fd9aef83f8550a91ba4a8094d246
DavidFFFan <DavidFFFan@foxmail.com>
2020-09-14 19:43:11 +0800

    导包方式改变，得到最优匹配的时间。

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import os
-from preprocessing import *
+import preprocessing as pp
 from processing import *
 import matplotlib.pyplot as plt
 
@@ -10,12 +10,13 @@ if __name__ == '__main__':
     # 标准工艺的时间，以及温度
     time, temperature = 30, 815
 
-    data = load_csv_data(path)
+    data = pp.load_csv_data(path)
+    
     # 打印实际数据曲线
     plt.plot(range(len(data)), data, 'bo:', label='current_tem', linewidth=1)
     
     # minDistance, minPos = minEdistance(tdata = data, stdTime= time, stdTemp=temperature)
     # print("minDistance", minDistance, "minPos", minPos)
-    minmaxEdistance(data, time, temperature)
+    print( pp.getTime(minmaxEdistance(data, time, temperature), pp.startT))
 
     plt.show()
